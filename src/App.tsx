9a731acf00652b816d6c5328a65ec6c8c2ac0205
Cedric Gui <cedric.guillas78@gmail.com>
2024-04-12 11:42:39 +0800

    feat(init): Initialize application

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+interface UserSession {
+  username: string;
+  token: string;
+}
+
+const App = () => {
+  return <div>Hello. Welcome to Hive Onboarding!</div>;
+};
+
+export const AppComponent = App;
---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createHashRouter, RouterProvider } from "react-router-dom";
+import { AppComponent } from "./App";
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <AppComponent />,
+  },
+]);
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
