400a5831eb9a33d97a777f5cdc2ae09b292e2b5d
theghost1980 <saturnob612@gmail.com>
2024-04-17 17:39:26 -0400

    update(on-boarding-tutorial): removed not necessary comp, IP:SR

--- src/pages/extension/extension-page.component.tsx
+++ src/pages/extension/extension-page.component.tsx
@@ -1,23 +1,11 @@
-import { useState } from "react";
 import { OnBoardTutorialComponent } from "../../components/on-board-tutorial/on-board-tutorial-component";
-import { Theme, ThemeContext } from "../../theme.context";
 import "./extension-page.component.scss";
 
 const ExtensionPage = () => {
-  const [theme, setTheme] = useState<Theme>(Theme.DARK);
-  const toggleTheme = () => {
-    setTheme((oldTheme) => {
-      return oldTheme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
-    });
-  };
   return (
-    <ThemeContext.Provider value={{ theme, setTheme, toggleTheme }}>
-      <div className={`theme ${theme}`}>
-        <div className="extension-page">
-          <OnBoardTutorialComponent isMobile={false} />
-        </div>
-      </div>
-    </ThemeContext.Provider>
+    <div className="extension-page">
+      <OnBoardTutorialComponent isMobile={false} />
+    </div>
   );
 };
 
--- src/pages/mobile/mobile-page.component.tsx
+++ src/pages/mobile/mobile-page.component.tsx
@@ -1,23 +1,11 @@
-import { useState } from "react";
 import { OnBoardTutorialComponent } from "../../components/on-board-tutorial/on-board-tutorial-component";
-import { Theme, ThemeContext } from "../../theme.context";
 import "./mobile-page.component.scss";
 
 const MobilePage = () => {
-  const [theme, setTheme] = useState<Theme>(Theme.DARK);
-  const toggleTheme = () => {
-    setTheme((oldTheme) => {
-      return oldTheme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
-    });
-  };
   return (
-    <ThemeContext.Provider value={{ theme, setTheme, toggleTheme }}>
-      <div className={`theme ${theme}`}>
-        <div className="mobile-page">
-          <OnBoardTutorialComponent isMobile={true} />
-        </div>
-      </div>
-    </ThemeContext.Provider>
+    <div className="mobile-page">
+      <OnBoardTutorialComponent isMobile={true} />
+    </div>
   );
 };
 
