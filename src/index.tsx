96da45be35fc2304078d00b79528c575f0515660
theghost1980 <saturnob612@gmail.com>
2024-04-17 17:43:34 -0400

    fix(on-boarding-tutorial): update hash router

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createHashRouter, HashRouter, Route, Routes } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import { AppComponent } from "./App";
 import { ErrorPageComponent } from "./pages/error-page/error-page.component";
 import { ExtensionPageComponent } from "./pages/extension/extension-page.component";
@@ -31,14 +31,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route path="/" Component={AppComponent} />
-        <Route path="extension" Component={ExtensionPageComponent} />
-        <Route path="mobile" Component={MobilePageComponent} />
-        <Route path="*" Component={ErrorPageComponent} />
-      </Routes>
-    </HashRouter>
-    {/* <RouterProvider router={router} /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
